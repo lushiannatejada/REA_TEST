303947cc2966951d746f92b8465d5f4cb2f60b29
lushiannatejada <lushiannatejada@gmail.com>
2020-06-06 01:06:35 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$(".chosen-select").chosen({
+		no_results_text: "Oops, no hay resultados!",
+		width: "100%"
+	});
+});
+
---
+++ precache-manifest.346f7ea409d5f3c8fd69349c4101dcab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7941ccb569b407684581ff589a5b724",
+    "url": "/REA_TEST/index.html"
+  },
+  {
+    "revision": "b65b10eeb8a65ecac731",
+    "url": "/REA_TEST/static/css/main.1c63e281.chunk.css"
+  },
+  {
+    "revision": "9939980fbf4c2f9d803c",
+    "url": "/REA_TEST/static/js/2.0b0a112d.chunk.js"
+  },
+  {
+    "revision": "eaa93899759cd78ff2a4147c0af30aa2",
+    "url": "/REA_TEST/static/js/2.0b0a112d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b65b10eeb8a65ecac731",
+    "url": "/REA_TEST/static/js/main.9eeedcf0.chunk.js"
+  },
+  {
+    "revision": "9b9d73b16557a61f0b70",
+    "url": "/REA_TEST/static/js/runtime-main.e1d693ca.js"
+  }
+]);
